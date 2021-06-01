f2dd9923ebaf968b70459f8c502c261a1ba8b606
mirciu-brendan <mirongrzywniak@gmail.com>
2021-06-01 21:19:56 +0200

    updating files

---
+++ src/app/submitValidation.js
@@ -0,0 +1,28 @@
+﻿
+function submit(values) {
+  
+  let validErrors = [];
+  const digit =/\d/;
+  const special = /\W/;
+    if (values.firstName.length<=2) validErrors.push(`Have you typed yor first name properly? It seems to be very short..`);
+    if (digit.test(values.firstName)) validErrors.push(`\nAre you sure your first name contains numbers?`);
+    if (special.test(values.firstName)) validErrors.push(`\nAre you sure your first name contains some special signs?`);
+    if (values.lastName.length>=10) validErrors.push(`\nYour first name has more than 10 sings?`);
+    if (values.lastName.length<=2) validErrors.push(`\nHave you typed yor last name properly? It seems to be very short..`);
+    if (digit.test(values.lastName)) validErrors.push(`\nAre you sure your last name contains numbers?`);
+    if (special.test(values.lastName)) validErrors.push(`\nAre you sure your last name contains some special signs?`);
+    if (values.lastName.length>=10) validErrors.push(`\nYour last name has more than 10 sings?`);
+    if (Date.parse(values.eventDate)<Date.now()) validErrors.push(`\nYou have selected a past date!`);
+        
+    if (validErrors.length===0){
+     
+        localStorage.setItem('DB', JSON.stringify(values)); 
+        console.log(values);
+      
+    }else{
+      window.alert(validErrors);
+    }   
+    
+};
+
+export default submit;
---
+++ src/components/EventForm.jsx
@@ -0,0 +1,43 @@
+﻿import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+import submit from '../app/submitValidation';
+
+// const renderField = ({ input, label, type, meta: { touched, error } }) => (
+//     <div>
+//       <label>{label}</label>
+//       <div>
+//         <input {...input} placeholder={label} type={type} />
+//         {touched && error && <span>{error}</span>}
+//       </div>
+//     </div>
+//   )
+
+let EventForm = props => {
+  const { handleSubmit } = props;
+  
+  
+  return (
+        <form onSubmit={handleSubmit(submit)}>
+            <div className="formDiv">
+                <Field name="firstName" component="input" type="text" placeHolder="First Name" required/>
+            </div>
+            <div className="formDiv">
+                <Field name="lastName" component="input" type="text" placeHolder="Last Name" required/>
+            </div>
+            <div className="formDiv">
+                <Field name="email" component="input" type="email"  placeHolder="Email" required/>
+            </div>
+            <div className="formDiv">
+                <label id="eventDate" htmlFor="eventDate">Event Date</label>
+                <Field name="eventDate" component="input" type="date" required/>
+            </div>
+            <button type="submit">Submit</button>
+        </form>
+    );
+};
+
+EventForm = reduxForm({
+  form: 'event'
+})(EventForm);
+
+export default EventForm;
